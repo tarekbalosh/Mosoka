cc01750ff5d73b54f4f7a37f5e849ff87eef6a6a
Tarek Balosh <73379464+tarekbalosh@users.noreply.github.com>
2024-04-24 12:21:46 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 $(".product1 , img").fadeIn(1500);
 
 
-$(".downorders  i  , .downorders i").click(function () {
+$(".down  i  , .downorders i").click(function () {
     $(".down input , .search ,.downorders .search").fadeToggle();
 
 });
